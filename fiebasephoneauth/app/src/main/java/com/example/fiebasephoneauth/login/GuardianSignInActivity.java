e8207eb5d86a62e5ee92e5fa442e5fce5444bf63
Kyu Tae Kim <104997803+ktkim604@users.noreply.github.com>
2023-11-02 02:42:18 +0900

    Add : DB 정보로 login 기능 구현

--- fiebasephoneauth/app/src/main/java/com/example/fiebasephoneauth/login/GuardianSignInActivity.java
+++ fiebasephoneauth/app/src/main/java/com/example/fiebasephoneauth/login/GuardianSignInActivity.java
@@ -1,16 +1,27 @@
 package com.example.fiebasephoneauth.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.fiebasephoneauth.R;
 import com.example.fiebasephoneauth.databinding.ActivityGuardianSignInBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class GuardianSignInActivity extends AppCompatActivity {
 
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://fir-phoneauth-97f7e-default-rtdb.firebaseio.com/");
     Button loginButton;
     TextView backButton;
 
@@ -25,13 +36,66 @@ public class GuardianSignInActivity extends AppCompatActivity {
         loginButton = binding.loginButton;
         backButton = binding.backButton;
 
-        loginButton.setOnClickListener(v -> {
-            // 로그인을 클릭할 경우
+
+        setContentView(R.layout.activity_guardian_sign_in);
+
+        final EditText id = findViewById(R.id.idForm);
+        final EditText password = findViewById(R.id.pwForm);
+        final Button loginBtn = findViewById(R.id.loginButton);
+        final TextView backBtn = findViewById(R.id.backButton);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GuardianSignInActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
         });
 
-        backButton.setOnClickListener(v -> {
-            Intent intent = new Intent(GuardianSignInActivity.this, MainActivity.class);
-            startActivity(intent);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final String idTxt = id.getText().toString();
+                final String passwordTxt = password.getText().toString();
+
+                if(idTxt.isEmpty() || passwordTxt.isEmpty()){
+                    Toast.makeText(GuardianSignInActivity.this, "모든 정보를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    databaseReference.child("Guardian_list").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            if(snapshot.hasChild(idTxt)){
+
+                                final String getPassword = snapshot.child(idTxt).child("password").getValue(String.class);
+
+                                if(getPassword.equals(passwordTxt)){
+                                    Toast.makeText(GuardianSignInActivity.this, "로그인 성공 !", Toast.LENGTH_SHORT).show();
+
+                                    startActivity(new Intent(GuardianSignInActivity.this, MainActivity.class));
+                                    finish();
+                                }
+
+                                else{
+                                    Toast.makeText(GuardianSignInActivity.this, "비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            else{
+                                Toast.makeText(GuardianSignInActivity.this, "아이디를 확인해주세요.", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+
+            }
         });
 
 

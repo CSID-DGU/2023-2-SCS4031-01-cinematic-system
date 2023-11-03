e1f004b631ece20251201196e1dec9a43ae6760c
minheonjun <tamunavollt1@gmail.com>
2023-11-04 01:09:32 +0900

    Add 보호자 이벤트 페이지 추가

--- fiebasephoneauth/app/src/main/java/com/example/fiebasephoneauth/Guardian/page/GuardianMenuEventFragment.java
+++ fiebasephoneauth/app/src/main/java/com/example/fiebasephoneauth/Guardian/page/GuardianMenuEventFragment.java
@@ -19,7 +19,13 @@ import com.example.fiebasephoneauth.R;
  */
 public class GuardianMenuEventFragment extends Fragment {
 
-    EventCardInfo[] eventCardInfo;
+
+    // 더미 데이터 (테스트용) -------------------------
+    EventCardInfo[] eventCardInfo = {
+        new EventCardInfo("2020-11-11", "12:00","fire"),
+        new EventCardInfo("2020-11-11", "12:00","fire"),
+    };
+    // ---------------------------------------------
 
     RecyclerView recyclerViewEventLog;
     RecyclerView.Adapter eventLogAdapter;
@@ -33,13 +39,12 @@ public class GuardianMenuEventFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_guardian_menu_event, container, false);
+
         recyclerViewEventLog = (RecyclerView) view.findViewById(R.id.recent_notification_recycler_view);
         recyclerViewEventLog.setHasFixedSize(true);
-
         eventLogAdapter = new HomeEventLogAdapter(eventCardInfo);
         recyclerViewEventLog.setAdapter(eventLogAdapter);
         recyclerViewEventLog.setLayoutManager(new LinearLayoutManager(getActivity()));
-
 
         return view;
     }
@@ -49,14 +54,50 @@ public class GuardianMenuEventFragment extends Fragment {
 class EventCardInfo {
     String date;
     String time;
-    String description;
-    String EmergencyType;
+    String title;
+    String description_short;
+    String description_long;
+    String emergencyType;
+    String imgSrc;
 
-    public EventCardInfo(String date, String time, String description, String EmergencyType) {
+    public EventCardInfo(String date, String time, String emerCode) {
         this.date = date;
         this.time = time;
-        this.EmergencyType = EmergencyType;
-        this.description = description;
+        emergencyType = emerCode;
+
+        switch(emerCode){
+            case "fire" :
+                title = "화재 발생";
+                description_short = "화재가 감지되어 자동신고 처리되었습니다.";
+                description_long = "화재 감지기를 통해 화재 상황이 감지되어 \n" +
+                        "담당자 전달 및 자동 신고 처리되었습니다. \n" +
+                        "아래 연락처를 통해 상황을 파악하시길 바랍니다.";
+                imgSrc = "fire";
+                break;
+            case "emergency" :
+                title = "응급 버튼 눌림";
+                description_short = "응급 버튼이 눌려 자동 신고 처리되었습니다.";
+                description_long = "응급 버튼이 눌렀습니다.\n" +
+                        "담당자에게 응급 상황이 전달되었으며,\n" +
+                        "아래 연락처를 통해 상황을 파악하시길 바랍니다.";
+                imgSrc = "emergency";
+                break;
+            case "no_movement_detected_1" :
+                title = "장기 미활동 감지";
+                description_short = "12시간 내의 활동이 감지되지 않았어요!";
+                description_long = "12시간 동안 활동이 감지되지 않았습니다. \n" +
+                        "아래 번호에 연락하여 안부를 여쭈어보세요.";
+                imgSrc = "no_movement";
+                break;
+            case "no_movement_detected_2" :
+                title = "장기 미활동 신고";
+                description_short = "장기 미활동 판단으로 신고처리되었습니다";
+                description_long = "24시간 활동이 감지되지 않아,\n" +
+                        "자동신고 접수가 되었습니다. ";
+                imgSrc = "no_movement";
+                break;
+        }
+
     }
 
     public String getDate() {
@@ -67,7 +108,24 @@ class EventCardInfo {
         return time;
     }
 
-    public String getDescription() {
-        return description;
+    public String getTitle() {
+        return title;
     }
+
+    public String getDescription_short() {
+        return description_short;
+    }
+
+    public String getDescription_long() {
+        return description_long;
+    }
+
+    public String getEmergencyType() {
+        return emergencyType;
+    }
+
+    public String getImgSrc() {
+        return imgSrc;
+    }
+
 }
--- fiebasephoneauth/app/src/main/java/com/example/fiebasephoneauth/Guardian/page/HomeEventLogAdapter.java
+++ fiebasephoneauth/app/src/main/java/com/example/fiebasephoneauth/Guardian/page/HomeEventLogAdapter.java
@@ -1,37 +1,58 @@
 package com.example.fiebasephoneauth.Guardian.page;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.fiebasephoneauth.R;
 
 public class HomeEventLogAdapter extends RecyclerView.Adapter<HomeEventLogAdapter.viewHolder> {
 
     EventCardInfo[] mData;
+
+
 
     public HomeEventLogAdapter(EventCardInfo[] eventCardInfo){
         this.mData = eventCardInfo;
     }
 
     class viewHolder extends RecyclerView.ViewHolder{
+
+        CardView eventLogCard;
+        TextView eventLogCardTitle;
+        TextView eventLogCardDescription;
+        ImageView eventLogCardIcon;
         public viewHolder(View itemView){
             super(itemView);
+            eventLogCard = itemView.findViewById(R.id.event_log_card);
+            eventLogCardIcon = itemView.findViewById(R.id.event_log_card_icon);
+            eventLogCardTitle = itemView.findViewById(R.id.event_log_card_title);
+            eventLogCardDescription = itemView.findViewById(R.id.event_log_card_description);
         }
     }
 
     @Override
     public HomeEventLogAdapter.viewHolder onCreateViewHolder(ViewGroup parent, int viewType){
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.event_log_card_view, parent, false);
+        return new viewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(HomeEventLogAdapter.viewHolder holder, int position){
-
+        holder.eventLogCardTitle.setText(mData[position].getTitle());
+        holder.eventLogCardDescription.setText(mData[position].getDescription_short());
+        holder.eventLogCardIcon.setImageResource(R.drawable.fire);
+        holder.eventLogCardIcon.setBackgroundColor(0xD64545);
     }
 
     @Override
     public int getItemCount(){
-        return 0;
+        return mData.length;
     }
 
 

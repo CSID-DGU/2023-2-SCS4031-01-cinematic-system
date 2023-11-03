3ea613edb56c8046baf89b9e212fb1a6c899b83b
Kyu Tae Kim <104997803+ktkim604@users.noreply.github.com>
2023-11-03 17:18:38 +0900

    Add : 정보 연동-> 추가작업 필요함

--- fiebasephoneauth/app/src/main/java/com/example/fiebasephoneauth/Guardian/connection/GuardianGetConnection.java
+++ fiebasephoneauth/app/src/main/java/com/example/fiebasephoneauth/Guardian/connection/GuardianGetConnection.java
@@ -1,14 +1,26 @@
 package com.example.fiebasephoneauth.Guardian.connection;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.fiebasephoneauth.Guardian.page.GuardianHome;
 import com.example.fiebasephoneauth.R;
 import com.example.fiebasephoneauth.databinding.ActivityGuardianGetConnectionBinding;
+import com.example.fiebasephoneauth.login.GuardianSignInActivity;
+import com.example.fiebasephoneauth.login.MainActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  *  <h3> 계정 연동 페이지 </h3>
@@ -16,6 +28,8 @@ import com.example.fiebasephoneauth.databinding.ActivityGuardianGetConnectionBin
  * 보호자가 계정 연동을 진행하는 페이지
  */
 public class GuardianGetConnection extends AppCompatActivity {
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://fir-phoneauth-97f7e-default-rtdb.firebaseio.com/");
 
     private ActivityGuardianGetConnectionBinding binding;
     @Override
@@ -30,6 +44,56 @@ public class GuardianGetConnection extends AppCompatActivity {
         Button requestAuthNumButton = binding.requestAuthNumButton;
         Button confirmAuthNumButton = binding.confirmAuthNumButton;
         TextView logoutText = binding.logoutText;
+        Button singup_button = binding.signupButton;
+
+        singup_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final String idTxt = nameForm.getText().toString();
+                final String phoneTxt = phoneForm.getText().toString();
+
+                if(idTxt.isEmpty() || phoneTxt.isEmpty()){
+                    Toast.makeText(GuardianGetConnection.this, "모든 정보를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    databaseReference.child("CareReceiver_list").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            if(snapshot.hasChild(idTxt)){
+
+                                final String getPassword = snapshot.child(idTxt).child("phoneNum").getValue(String.class);
+
+                                if(getPassword.equals(phoneTxt)){
+                                    Toast.makeText(GuardianGetConnection.this, "연동 성공 !", Toast.LENGTH_SHORT).show();
+
+                                    Intent intent = new Intent(GuardianGetConnection.this, GuardianHome.class);
+                                    intent.putExtra("id",idTxt);
+                                    startActivity(intent);
+                                    finish();
+                                }
+
+                                else{
+                                    Toast.makeText(GuardianGetConnection.this, "전화번호를 다시 확인해주세요.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            else{
+                                Toast.makeText(GuardianGetConnection.this, "보호자 아이디를 확인해주세요.", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+
+            }
+        });
+
 
     }
 }

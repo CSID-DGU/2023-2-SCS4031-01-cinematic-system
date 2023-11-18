033fb1eaa3b2178a92a3f9e50b2cc4877249c4f3
Kyu Tae Kim <104997803+ktkim604@users.noreply.github.com>
2023-11-19 01:51:53 +0900

    Fix : 백그라운드 푸시 알림 기능 구현(고도화 필요)

--- fiebasephoneauth/app/src/main/java/com/example/fiebasephoneauth/MyFirebaseMessagingService.java
+++ fiebasephoneauth/app/src/main/java/com/example/fiebasephoneauth/MyFirebaseMessagingService.java
@@ -1,17 +1,70 @@
 package com.example.fiebasephoneauth;
 
-import android.content.Intent;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.util.Log;
 
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import androidx.core.app.NotificationCompat;
 
-import com.example.fiebasephoneauth.Guardian.page.GuardianHome;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Random;
+
+
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+
+        Log.d("MSG",remoteMessage.getNotification().getBody());
+        shownotification(remoteMessage.getNotification());
+
+
+    }
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        Log.d("NEW_TOKEN",s);
+    }
+    public void shownotification(RemoteMessage.Notification message){
+
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "com.example.fiebasephoneauth"; //your app package name
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Notification",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            notificationChannel.setDescription("Techrush Channel");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.emer)
+                .setContentTitle(message.getTitle())
+                .setContentText(message.getBody())
+                .setContentInfo("Info");
+
+        notificationManager.notify(new Random().nextInt(),notificationBuilder.build());
+
+    }
 
 }
+
+
 
 
 

b6aa1b4b503410858b2092151a7a5fd1d6da5b89
minheonjun <tamunavollt1@gmail.com>
2023-12-02 17:22:10 +0900

    Add : 화재알림 피보호자에게 푸시알림 전송 후 로그 기록

--- CloudFunction/functions/index.js
+++ CloudFunction/functions/index.js
@@ -123,16 +123,42 @@ exports.checkEmergency = functions.database.ref("/CareReceiver_list/{userId}/Act
 // eslint-disable-next-line max-len
 exports.checkFire = functions.database.ref("/CareReceiver_list/{userId}/ActivityData/fire")
     .onUpdate((snapshot, context) => {
-      const fireNewValue = snapshot.after.val();
+      const fireValue = snapshot.after.val();
       const userId = context.params.userId;
       console.log("userId: ", userId);
 
-      if (fireNewValue === "1") {
+      if (fireValue === "1") {
+        const careReceiverDataRef = admin.database().ref(`/CareReceiver_list/${userId}`);
         const guardianDataRef = admin.database().ref("/Guardian_list/");
 
+          // 피보호자에게 푸시 알림을 보냄
+          careReceiverDataRef.once("value").then((careReceiverSnapshot) => {
+              const careReceiverData = careReceiverSnapshot.val();
+              const tokenObject = careReceiverData.deviceToken;
+              console.log("tokenObject: ", tokenObject);
+
+              for (const key in tokenObject) {
+                  if (Object.prototype.hasOwnProperty.call(tokenObject, key)) {
+                      const token = tokenObject[key];
+                      const message = {
+                          notification: {
+                              title: "화재 상황 발생",
+                              body: "화재 감자기에서 화재를 감지했습니다",
+                          },
+                          token: token,
+                      };
+                      admin.messaging().send(message).then((response) => {
+                          console.log("Message sent successfully:", response, "token: ", token);
+                      })
+                          .catch((error) => {
+                              console.log("Error sending message: ", error);
+                          });
+                  }
+              }
+          });
+
         // 피보호자의 보호자를 찾아서 푸시 알림을 보냄
-        return guardianDataRef.once("value").then((guardianListSnapshot) => {
-          const promises = []; // To hold all promises
+        guardianDataRef.once("value").then((guardianListSnapshot) => {
 
           guardianListSnapshot.forEach((guardianSnapshot) => {
             const guardianData = guardianSnapshot.val();
@@ -159,24 +185,44 @@ exports.checkFire = functions.database.ref("/CareReceiver_list/{userId}/Activity
                       },
                       token: token,
                     };
-                    promises.push(
-                        admin.messaging().send(message).then((response) => {
-                          // eslint-disable-next-line max-len
-                          console.log("Message sent successfully:", response, "token: ", token);
-                        })
-                            .catch((error) => {
-                              console.log("Error sending message: ", error);
-                            }),
+                    admin.messaging().send(message).then((response) => {
+                      // eslint-disable-next-line max-len
+                      console.log("Message sent successfully:", response, "token: ", token);
+                    })
+                        .catch((error) => {
+                          console.log("Error sending message: ", error);
+                        }
                     );
                   }
                 }
               });
             }
           });
-          return Promise.all(promises);
         });
-      } else {
-        return Promise.resolve();
+        // 화재상황 기록
+        careReceiverDataRef.child("ActivityData").child("latestEvent").once("value").then((latestEventSnapshot) => {
+            const latestEvent = latestEventSnapshot.val();
+            const keys = Object.keys(latestEvent);
+            const numChildren = Object.keys(latestEvent).length;
+            console.log("numChildren: ", numChildren);
+            // 최근 4개의 기록만 남기고 나머지는 삭제
+            if (numChildren > 4) {
+                  keys.sort();
+                  const oldestKey = keys[0];
+                  careReceiverDataRef.child("ActivityData").child("latestEvent").child(oldestKey).remove();
+            }
+        });
+        const fireTimeStamp = Date.now();
+        const latestEvent = admin.database().ref(`/CareReceiver_list/${userId}/ActivityData/latestEvent`);
+        latestEvent.push(
+              {
+                  time : fireTimeStamp,
+                  type : "fire"
+              }
+        );
+
+        // 화재상황 발생 시 피보호자의 화재상황 발생 여부를 0으로 초기화
+        careReceiverDataRef.child("ActivityData").child("fire").set("0");
       }
     });
 

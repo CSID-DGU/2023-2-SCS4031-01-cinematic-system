811a70948bfb9ab9f73e4d7a31018d15b869ca1a
minheonjun <tamunavollt1@gmail.com>
2023-12-07 19:26:05 +0900

    Fix : 응급버튼 타이머 추가

--- fiebasephoneauth/app/src/main/java/com/example/fiebasephoneauth/CareReceiver/page/CareReceiverIsEmerQuery.java
+++ fiebasephoneauth/app/src/main/java/com/example/fiebasephoneauth/CareReceiver/page/CareReceiverIsEmerQuery.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,6 +21,10 @@ public class CareReceiverIsEmerQuery extends AppCompatActivity {
 
     private ActivityCareReceiverIsEmerQueryBinding binding;
 
+    Button cancelButton;
+
+    TextView timeSet;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +36,34 @@ public class CareReceiverIsEmerQuery extends AppCompatActivity {
         String idTxt = intent.getStringExtra("id");
 
         // 레이아웃 요소들
-        Button cancelButton = binding.cancelButton;
+        cancelButton = binding.cancelButton;
+        timeSet = binding.timeSet;
+
+        // timeSet 15초 타이머 설정, 1초마다 1씩 감소
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i=15; i>=0; i--){
+                    try {
+                        final int finalI = i;
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                timeSet.setText(String.valueOf(finalI) + "초 이내에 응답해주세요.");
+                            }
+                        });
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Intent intent1 = new Intent(CareReceiverIsEmerQuery.this, CareReceiverEventLog.class);
+                intent1.putExtra("id",idTxt);
+                startActivity(intent1);
+            }
+        });
+
+        thread.start();
 
 
         cancelButton.setOnClickListener(new View.OnClickListener() {
